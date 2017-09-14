1d16ad58fa433d4d11dd26941bf00bbbf667a1c4
Zheng Yang <zhengyang@rock-chips.com>
2017-09-14 10:37:37 +0800

    drm: rockchip: hdmi: attach HDR_SOURCE_METADATA property
    HDR_SOURCE_METADATA property is used to set source hdr metadata,
    which will be sent to sink though HDMI DRM infoframe.
    
    Change-Id: If3500cb505c16c2f0caf66b8e64b4d80b93b228f
    Signed-off-by: Zheng Yang <zhengyang@rock-chips.com>

--- drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
+++ drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
@@ -807,6 +807,10 @@ dw_hdmi_rockchip_attach_properties(struct drm_connector *connector,
 		hdmi->quant_range = prop;
 		drm_object_attach_property(&connector->base, prop, 0);
 	}
+
+	prop = connector->dev->mode_config.hdr_output_metadata_property;
+	if (version >= 0x211a)
+		drm_object_attach_property(&connector->base, prop, 0);
 }
 
 static void
@@ -854,6 +858,7 @@ dw_hdmi_rockchip_set_property(struct drm_connector *connector,
 			      void *data)
 {
 	struct rockchip_hdmi *hdmi = (struct rockchip_hdmi *)data;
+	struct drm_mode_config *config = &connector->dev->mode_config;
 
 	if (property == hdmi->color_depth_property) {
 		hdmi->colordepth = val;
@@ -863,6 +868,8 @@ dw_hdmi_rockchip_set_property(struct drm_connector *connector,
 		return 0;
 	} else if (property == hdmi->quant_range) {
 		hdmi->hdmi_quant_range = val;
+		return 0;
+	} else if (property == config->hdr_output_metadata_property) {
 		return 0;
 	}
 
@@ -879,6 +886,7 @@ dw_hdmi_rockchip_get_property(struct drm_connector *connector,
 {
 	struct rockchip_hdmi *hdmi = (struct rockchip_hdmi *)data;
 	struct drm_display_info *info = &connector->display_info;
+	struct drm_mode_config *config = &connector->dev->mode_config;
 
 	if (property == hdmi->color_depth_property) {
 		*val = hdmi->colordepth;
@@ -916,6 +924,10 @@ dw_hdmi_rockchip_get_property(struct drm_connector *connector,
 		return 0;
 	} else if (property == hdmi->quant_range) {
 		*val = hdmi->hdmi_quant_range;
+		return 0;
+	} else if (property == config->hdr_output_metadata_property) {
+		*val = state->hdr_output_metadata ?
+			state->hdr_output_metadata->base.id : 0;
 		return 0;
 	}
 
