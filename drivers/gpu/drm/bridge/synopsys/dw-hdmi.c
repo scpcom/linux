c092baf02992dd23b1891e9420e3fd3f84c42a45
Sandy Huang <hjc@rock-chips.com>
2020-07-01 17:34:57 +0800

    drm/bridge: synopsys: dw-hdmi: update for remove connector port
    Change-Id: Ia0ca8c2fddf89f29bf4ac5703d8f4d0f68d6446a
    Signed-off-by: Sandy Huang <hjc@rock-chips.com>

--- drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
+++ drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
@@ -4198,7 +4198,7 @@ EXPORT_SYMBOL_GPL(dw_hdmi_remove);
  */
 struct dw_hdmi *dw_hdmi_bind(struct platform_device *pdev,
 			     struct drm_encoder *encoder,
-			     const struct dw_hdmi_plat_data *plat_data)
+			     struct dw_hdmi_plat_data *plat_data)
 {
 	struct dw_hdmi *hdmi;
 	int ret;
@@ -4212,6 +4212,7 @@ struct dw_hdmi *dw_hdmi_bind(struct platform_device *pdev,
 		dw_hdmi_remove(hdmi);
 		return ERR_PTR(ret);
 	}
+	plat_data->connector = &hdmi->connector;
 
 	return hdmi;
 }
