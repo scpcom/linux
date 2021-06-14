101ad1a8c8ce5cca4632825d78c99ccaa5436c42
Samuel Holland <samuel@sholland.org>
2021-06-13 23:53:26 -0500

    ASoC: sun4i-spdif: Add support for the D1 variant
    The D1 variant is similar to the H6 variant, except for its clock setup.
    The clock tree changes impact some register fields on the RX side, but
    those are not yet relevant, because RX is not supported by this driver.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- sound/soc/sunxi/sun4i-spdif.c
+++ sound/soc/sunxi/sun4i-spdif.c
@@ -561,6 +561,13 @@ static const struct sun4i_spdif_quirks sun8i_h3_spdif_quirks = {
 	.mclk_multiplier = 4,
 };
 
+static const struct sun4i_spdif_quirks sun20i_d1_spdif_quirks = {
+	.tx_clk_name	= "tx",
+	.reg_dac_txdata = SUN8I_SPDIF_TXFIFO,
+	.val_fctl_ftx   = SUN50I_H6_SPDIF_FCTL_FTX,
+	.mclk_multiplier = 1,
+};
+
 static const struct sun4i_spdif_quirks sun50i_h6_spdif_quirks = {
 	.tx_clk_name	= "spdif",
 	.reg_dac_txdata = SUN8I_SPDIF_TXFIFO,
@@ -580,6 +587,10 @@ static const struct of_device_id sun4i_spdif_of_match[] = {
 	{
 		.compatible = "allwinner,sun8i-h3-spdif",
 		.data = &sun8i_h3_spdif_quirks,
+	},
+	{
+		.compatible = "allwinner,sun20i-d1-spdif",
+		.data = &sun20i_d1_spdif_quirks,
 	},
 	{
 		.compatible = "allwinner,sun50i-h6-spdif",
