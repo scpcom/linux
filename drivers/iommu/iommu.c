ef2db7fb1bace184978c1bb19c267c56e2cfce43
Simon Xue <xxm@rock-chips.com>
2021-12-07 10:10:19 +0800

    Revert "drm/rockchip: gem: add IOMMU_TLB_SHOT_ENTIRE when call iommu_map_sg"
    This reverts commit 91db0733d7cf2c83053c5eef8403a2ea8bcfca32.
    
    Signed-off-by: Simon Xue <xxm@rock-chips.com>
    Change-Id: I7842de3f5a7e4a64e570ceb93e82fdee38f78498

--- drivers/iommu/iommu.c
+++ drivers/iommu/iommu.c
@@ -2666,11 +2666,6 @@ next:
 			goto out_err;
 	}
 
-#ifdef IOMMU_TLB_SHOT_ENTIRE
-	if (domain->ops->flush_iotlb_all && (prot & IOMMU_TLB_SHOT_ENTIRE))
-		domain->ops->flush_iotlb_all(domain);
-#endif
-
 	return mapped;
 
 out_err:
