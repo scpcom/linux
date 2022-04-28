037bd8bd137e821e49ded6fe1eaa4e61689388b4
Samuel Holland <samuel@sholland.org>
2022-04-27 19:01:57 -0500

    iommu/sun50i: Support variants without an external reset
    The IOMMU in the Allwinner D1 SoC does not have an external reset line.
    
    Only attempt to get the reset on hardware variants which should have one
    according to the binding. And switch from the deprecated function to the
    explicit "exclusive" variant.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/iommu/sun50i-iommu.c
+++ drivers/iommu/sun50i-iommu.c
@@ -97,6 +97,10 @@
 
 #define SPAGE_SIZE			4096
 
+struct sun50i_iommu_variant {
+	bool has_reset;
+};
+
 struct sun50i_iommu {
 	struct iommu_device iommu;
 
@@ -995,8 +999,13 @@ static irqreturn_t sun50i_iommu_irq(int irq, void *dev_id)
 
 static int sun50i_iommu_probe(struct platform_device *pdev)
 {
+	const struct sun50i_iommu_variant *variant;
 	struct sun50i_iommu *iommu;
 	int ret, irq;
+
+	variant = of_device_get_match_data(&pdev->dev);
+	if (!variant)
+		return -EINVAL;
 
 	iommu = devm_kzalloc(&pdev->dev, sizeof(*iommu), GFP_KERNEL);
 	if (!iommu)
@@ -1032,7 +1041,8 @@ static int sun50i_iommu_probe(struct platform_device *pdev)
 		goto err_free_cache;
 	}
 
-	iommu->reset = devm_reset_control_get(&pdev->dev, NULL);
+	if (variant->has_reset)
+		iommu->reset = devm_reset_control_get_exclusive(&pdev->dev, NULL);
 	if (IS_ERR(iommu->reset)) {
 		dev_err(&pdev->dev, "Couldn't get our reset line.\n");
 		ret = PTR_ERR(iommu->reset);
@@ -1067,9 +1077,13 @@ err_free_cache:
 	return ret;
 }
 
+static const struct sun50i_iommu_variant sun50i_h6_iommu = {
+	.has_reset = true,
+};
+
 static const struct of_device_id sun50i_iommu_dt[] = {
-	{ .compatible = "allwinner,sun50i-h6-iommu", },
-	{ .compatible = "allwinner,sun50i-h616-iommu", },
+	{ .compatible = "allwinner,sun50i-h6-iommu", .data = &sun50i_h6_iommu },
+	{ .compatible = "allwinner,sun50i-h616-iommu", .data = &sun50i_h6_iommu },
 	{ /* sentinel */ },
 };
 MODULE_DEVICE_TABLE(of, sun50i_iommu_dt);
