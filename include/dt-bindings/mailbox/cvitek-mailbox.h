4505f37bd51a69494f94a9303c4c055bf2498907
Justin Hammond <justin@dynam.ac>
2024-04-06 14:27:58 +0800

    Add Mailbox Driver for cvitek

---
+++ include/dt-bindings/mailbox/cvitek-mailbox.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause */
+/*
+ * Copyright (c) 2018-2020, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __DT_BINDINGS_MAILBOX_IPCC_H
+#define __DT_BINDINGS_MAILBOX_IPCC_H
+
+#define CVITEK_MBOX_DIRECTION_RX 1
+#define	CVITEK_MBOX_DIRECTION_TX 2
+
+#define	CVITEK_MBOX_C906_0 1
+#define	CVITEK_MBOX_C906_1 2
+
+#endif
