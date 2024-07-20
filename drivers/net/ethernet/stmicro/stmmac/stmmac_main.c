d183e92e52c30a64c5fd23b49c920ebdf10d322b
sophgo-forum-service <forum_service@sophgo.com>
2024-07-20 21:41:42 +0800

    linux_5.10: weekly rls 2024.07.20
    -142736, Sync spacc driver code.
    -2303c6, add suspend to ram featrue.
    -d3bff8, support pdm mic using AEC.
    -203610, support new spinand flash:HYF1GQ4UDACAE.
    
    Change-Id: Iec21bccaee43c4a71213b42b88f41a45527fd8a5

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -7122,6 +7122,22 @@ static const struct net_device_ops stmmac_netdev_ops = {
 	.ndo_xsk_wakeup = stmmac_xsk_wakeup,
 };
 
+void stmmac_reset_subtask2(struct stmmac_priv *priv)
+{
+	rtnl_lock();
+	netif_trans_update(priv->dev);
+	while (test_and_set_bit(STMMAC_RESETTING, &priv->state))
+		usleep_range(1000, 2000);
+
+	set_bit(STMMAC_DOWN, &priv->state);
+	dev_close(priv->dev);
+	dev_open(priv->dev, NULL);
+	clear_bit(STMMAC_DOWN, &priv->state);
+	clear_bit(STMMAC_RESETTING, &priv->state);
+	rtnl_unlock();
+}
+EXPORT_SYMBOL_GPL(stmmac_reset_subtask2);
+
 static void stmmac_reset_subtask(struct stmmac_priv *priv)
 {
 	if (!test_and_clear_bit(STMMAC_RESET_REQUESTED, &priv->state))
@@ -7133,14 +7149,14 @@ static void stmmac_reset_subtask(struct stmmac_priv *priv)
 
 	rtnl_lock();
 	netif_trans_update(priv->dev);
-	while (test_and_set_bit(STMMAC_RESETING, &priv->state))
+	while (test_and_set_bit(STMMAC_RESETTING, &priv->state))
 		usleep_range(1000, 2000);
 
 	set_bit(STMMAC_DOWN, &priv->state);
 	dev_close(priv->dev);
 	dev_open(priv->dev, NULL);
 	clear_bit(STMMAC_DOWN, &priv->state);
-	clear_bit(STMMAC_RESETING, &priv->state);
+	clear_bit(STMMAC_RESETTING, &priv->state);
 	rtnl_unlock();
 }
 
@@ -8043,6 +8059,8 @@ int stmmac_resume(struct device *dev)
 	rtnl_unlock();
 
 	netif_device_attach(ndev);
+	if (ndev->phydev)
+		phy_start(ndev->phydev);
 
 	return 0;
 }
