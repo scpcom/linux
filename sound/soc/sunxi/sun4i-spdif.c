0b17b87babf7e318eb923702555d90293becb5c0
scpcom <scpcom@gmx.de>
2026-07-15 19:00:21 +0200

    fix merge "ASoC: sun4i-spdif: Add clock multiplier settings"

--- sound/soc/sunxi/sun4i-spdif.c
+++ sound/soc/sunxi/sun4i-spdif.c
@@ -553,24 +553,28 @@ static const struct sun4i_spdif_quirks sun6i_a31_spdif_quirks = {
 	.tx_clk_name	= "spdif",
 	.reg_dac_txdata	= SUN4I_SPDIF_TXFIFO,
 	.val_fctl_ftx   = SUN4I_SPDIF_FCTL_FTX,
+	.mclk_multiplier = 1,
 };
 
 static const struct sun4i_spdif_quirks sun8i_h3_spdif_quirks = {
 	.tx_clk_name	= "spdif",
 	.reg_dac_txdata	= SUN8I_SPDIF_TXFIFO,
 	.val_fctl_ftx   = SUN4I_SPDIF_FCTL_FTX,
+	.mclk_multiplier = 4,
 };
 
 static const struct sun4i_spdif_quirks sun20i_d1_spdif_quirks = {
 	.tx_clk_name	= "tx",
 	.reg_dac_txdata = SUN8I_SPDIF_TXFIFO,
 	.val_fctl_ftx   = SUN50I_H6_SPDIF_FCTL_FTX,
+	.mclk_multiplier = 1,
 };
 
 static const struct sun4i_spdif_quirks sun50i_h6_spdif_quirks = {
 	.tx_clk_name	= "spdif",
 	.reg_dac_txdata = SUN8I_SPDIF_TXFIFO,
 	.val_fctl_ftx   = SUN50I_H6_SPDIF_FCTL_FTX,
+	.mclk_multiplier = 1,
 };
 
 static const struct of_device_id sun4i_spdif_of_match[] = {
