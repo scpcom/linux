535f2609e2caa85523513c4e7a68269049849db5
scpcom <scpcom@gmx.de>
2026-07-17 01:38:09 +0200

    arm: midgard r14p0: include minmax.h

--- drivers/gpu/arm/midgard_for_linux/mali_malisw.h
+++ drivers/gpu/arm/midgard_for_linux/mali_malisw.h
@@ -23,6 +23,9 @@
 #define _MALISW_H_
 
 #include <linux/version.h>
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0)
+#include <linux/minmax.h>
+#endif
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 14, 0)
 #define U8_MAX          ((u8)~0U)
 #define S8_MAX          ((s8)(U8_MAX>>1))
@@ -47,7 +50,9 @@
  * As a macro it may evaluate its arguments more than once.
  * Refer to MAX macro for more details
  */
+#ifndef MIN
 #define MIN(x, y)	((x) < (y) ? (x) : (y))
+#endif
 
 /**
  * MAX -  Return the greater of two values.
@@ -59,7 +64,9 @@
  * to retrieve the min and max of two values, consider using a conditional swap
  * instead.
  */
+#ifndef MAX
 #define MAX(x, y)	((x) < (y) ? (y) : (x))
+#endif
 
 /**
  * @hideinitializer
