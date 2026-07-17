b6dd3a338e8c799dec46929a8c0c3df2914dd086
scpcom <scpcom@gmx.de>
2026-07-18 00:11:21 +0200

    phy: ls1024a: serdes: include linux/of_device.h

--- drivers/phy/freescale/phy-ls1024a-serdes.c
+++ drivers/phy/freescale/phy-ls1024a-serdes.c
@@ -5,6 +5,7 @@
 #include <linux/kernel.h>
 #include <linux/mfd/syscon.h>
 #include <linux/module.h>
+#include <linux/of_device.h>
 #include <linux/of_platform.h>
 #include <linux/phy/phy.h>
 #include <linux/platform_device.h>
