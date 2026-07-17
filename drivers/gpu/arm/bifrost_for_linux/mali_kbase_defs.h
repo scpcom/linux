e8e095b9f07f76cb32dcd07214cb4aad30a168de
scpcom <scpcom@gmx.de>
2026-07-17 02:08:46 +0200

    arm: bifrost: r8p0: dynamically allocate the shrinker

--- drivers/gpu/arm/bifrost_for_linux/mali_kbase_defs.h
+++ drivers/gpu/arm/bifrost_for_linux/mali_kbase_defs.h
@@ -947,7 +947,11 @@ struct kbase_mem_pool {
 	size_t		    order;
 	spinlock_t          pool_lock;
 	struct list_head    page_list;
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 7, 0)
 	struct shrinker     reclaim;
+#else
+	struct shrinker     *reclaim;
+#endif
 
 	struct kbase_mem_pool *next_pool;
 };
@@ -1470,7 +1474,11 @@ struct kbase_context {
 	struct kbase_mem_pool mem_pool;
 	struct kbase_mem_pool lp_mem_pool;
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 7, 0)
 	struct shrinker         reclaim;
+#else
+	struct shrinker         *reclaim;
+#endif
 	struct list_head        evict_list;
 
 	struct list_head waiting_soft_jobs;
