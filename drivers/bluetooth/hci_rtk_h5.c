233e704cffd471a0d06382c8ecdbeb3cbf638c26
scpcom <scpcom@gmx.de>
2026-07-18 19:24:58 +0200

    bluetooth: hci_rtk_h5: include linux/unaligned.h

--- drivers/bluetooth/hci_rtk_h5.c
+++ drivers/bluetooth/hci_rtk_h5.c
@@ -33,7 +33,7 @@
 #include <linux/ioctl.h>
 #include <linux/skbuff.h>
 #include <linux/bitrev.h>
-#include <asm/unaligned.h>
+#include <linux/unaligned.h>
 #include <net/bluetooth/bluetooth.h>
 #include <net/bluetooth/hci_core.h>
 #include <linux/version.h>
