d41e3aa30c3bbcfaaf7798284e7f14c418c79545
scpcom <scpcom@gmx.de>
2026-07-18 19:31:43 +0200

    mtd: spi-nor: xtx: convert flash_info to new format

--- drivers/mtd/spi-nor/xtx.c
+++ drivers/mtd/spi-nor/xtx.c
@@ -8,10 +8,10 @@
 #include "core.h"
 
 static const struct flash_info xtx_parts[] = {
-	{ "XT25F64F", INFO(0x0b4017, 0, 64 * 1024, 128)
-		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
-	{ "XT25F128B", INFO(0x0b4018, 0, 64 * 1024, 256)
-		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	{ .name = "XT25F64F", .id = SNOR_ID(0x0b, 0x40, 0x17), .size = SZ_8M,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ },
+	{ .name = "XT25F128B", .id = SNOR_ID(0x0b, 0x40, 0x18), .size = SZ_16M,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ },
 };
 
 const struct spi_nor_manufacturer spi_nor_xtx = {
