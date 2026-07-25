d960cf7ad72fdabb05f3d6ac75468190475828d6
scpcom <scpcom@gmx.de>
2026-07-25 18:27:24 +0200

    video: rockchip: rga2: remove return value from rga2_drv_remove

--- drivers/video/rockchip/rga2/rga2_drv.c
+++ drivers/video/rockchip/rga2/rga2_drv.c
@@ -1858,7 +1858,7 @@ err_ioremap:
 	return ret;
 }
 
-static int rga2_drv_remove(struct platform_device *pdev)
+static void rga2_drv_remove(struct platform_device *pdev)
 {
 	struct rga2_drvdata_t *data = platform_get_drvdata(pdev);
 	DBG("%s [%d]\n",__FUNCTION__,__LINE__);
@@ -1880,7 +1880,6 @@ static int rga2_drv_remove(struct platform_device *pdev)
 #endif
 
 	//kfree(data);
-	return 0;
 }
 
 static struct platform_driver rga2_driver = {
