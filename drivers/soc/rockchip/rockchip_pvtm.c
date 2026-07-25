28a55423ad02ae9134803ca7fede48a783d5a9d9
scpcom <scpcom@gmx.de>
2026-07-25 18:01:59 +0200

    soc: rockchip: pvtm: include of_device.h

--- drivers/soc/rockchip/rockchip_pvtm.c
+++ drivers/soc/rockchip/rockchip_pvtm.c
@@ -15,6 +15,7 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_clk.h>
+#include <linux/of_device.h>
 #include <linux/of_platform.h>
 #include <linux/platform_device.h>
 #include <linux/regmap.h>
