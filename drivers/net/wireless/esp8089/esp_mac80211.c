2338728aea64b49a76f1dd6e1846748750980081
scpcom <scpcom@gmx.de>
2026-07-31 01:15:47 +0200

    wireless: esp8089: simplify non-chanctx drivers

--- drivers/net/wireless/esp8089/esp_mac80211.c
+++ drivers/net/wireless/esp8089/esp_mac80211.c
@@ -1986,6 +1986,12 @@ static const struct ieee80211_ops esp_mac80211_ops = {
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 2, 0))
 	.wake_tx_queue = ieee80211_handle_wake_tx_queue,
 #endif
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 9, 0))
+	.add_chanctx = ieee80211_emulate_add_chanctx,
+	.remove_chanctx = ieee80211_emulate_remove_chanctx,
+	.change_chanctx = ieee80211_emulate_change_chanctx,
+	.switch_vif_chanctx = ieee80211_emulate_switch_vif_chanctx,
+#endif
 };
 
 struct esp_pub * esp_pub_alloc_mac80211(struct device *dev)
