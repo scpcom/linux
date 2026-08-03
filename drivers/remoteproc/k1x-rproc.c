4638b4b39f519e2366f5d7ce12797cca2bcbaf91
scpcom <scpcom@gmx.de>
2026-08-04 00:18:10 +0200

    remoteproc: k1x: include linux/platform_device.h

--- drivers/remoteproc/k1x-rproc.c
+++ drivers/remoteproc/k1x-rproc.c
@@ -7,6 +7,7 @@
 #include <linux/mfd/syscon.h>
 #include <linux/of_device.h>
 #include <linux/of_reserved_mem.h>
+#include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
 #include <linux/remoteproc.h>
 #include <linux/reset.h>
