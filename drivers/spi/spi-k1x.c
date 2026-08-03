5318e7690a60404b0e876b5f01e3d611fe1758bf
scpcom <scpcom@gmx.de>
2026-08-04 00:54:00 +0200

    spi: k1x: remove return value from k1x_spi_remove

--- drivers/spi/spi-k1x.c
+++ drivers/spi/spi-k1x.c
@@ -1037,12 +1037,12 @@ out_error_master_alloc:
 	return status;
 }
 
-static int k1x_spi_remove(struct platform_device *pdev)
+static void k1x_spi_remove(struct platform_device *pdev)
 {
 	struct spi_driver_data *drv_data = platform_get_drvdata(pdev);
 
 	if (!drv_data)
-		return 0;
+		return;
 
 	pm_runtime_get_sync(&pdev->dev);
 
@@ -1064,15 +1064,11 @@ static int k1x_spi_remove(struct platform_device *pdev)
 	free_irq(drv_data->irq, drv_data);
 
 	deinit_dvfm_constraint(drv_data);
-	return 0;
 }
 
 static void k1x_spi_shutdown(struct platform_device *pdev)
 {
-	int status = 0;
-
-	if ((status = k1x_spi_remove(pdev)) != 0)
-		dev_err(&pdev->dev, "shutdown failed with %d\n", status);
+	k1x_spi_remove(pdev);
 }
 
 #ifdef CONFIG_PM_SLEEP
