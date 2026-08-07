ed8fe834c90b328555995ace362c72c5a5e86ca5
scpcom <scpcom@gmx.de>
2026-08-07 21:07:29 +0200

    drm/rockchip: rgb: include linux/platform_device.h

--- drivers/gpu/drm/rockchip/rockchip_rgb.c
+++ drivers/gpu/drm/rockchip/rockchip_rgb.c
@@ -9,6 +9,7 @@
 #include <linux/media-bus-format.h>
 #include <linux/of_device.h>
 #include <linux/of_graph.h>
+#include <linux/platform_device.h>
 #include <linux/regmap.h>
 #include <linux/mfd/syscon.h>
 #include <linux/phy/phy.h>
