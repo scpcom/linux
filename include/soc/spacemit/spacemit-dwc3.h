73b9910167e7d38decb90c4be7f9b81eb55d672d
scpcom <scpcom@gmx.de>
2026-08-10 18:38:58 +0200

    usb: dwc3: spacemit: fix "warning: no previous prototype"

---
+++ include/soc/spacemit/spacemit-dwc3.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * dwc3-spacemit.c - Spacemit DWC3 Specific Glue layer
+ *
+ * Copyright (c) 2023 Spacemit Co., Ltd.
+ *
+ */
+#ifndef __SPACEMIT_DWC3_H__
+#define __SPACEMIT_DWC3_H__
+
+void dwc3_spacemit_clear_disconnect(struct device *dev);
+
+#endif
